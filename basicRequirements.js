2555d6eefc1c03df1e44e0b125bedca20bc8e55a
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 20:59:56 -0700

    completed most of basic requirements

---
+++ basicRequirements.js
@@ -0,0 +1,23 @@
+/* 
+  1) Display messages retrieved from the parse server. - DONE
+
+  2) Setup a way to refresh the displayed messages (either automatically or with a button). - DONE
+
+  3) Be careful to use proper escaping on any user input. Since you're
+  displaying input that other users have typed, your app is vulnerable
+  XSS attacks.
+  Note: If you issue an XSS attack, please make it innocuous enough to be educational, rather 
+  than disruptive. -- DONE
+
+  4) Allow users to select a username and send messages -- DONE
+
+  5) Allow users to create rooms and enter existing rooms -- DONE
+
+  6) Allow users to 'befriend' other users by clicking on their 
+  username
+
+  7) Display all messages sent by friends in bold
+
+  8) Complete the Hack Reactor Backbone intro (you have access 
+  to this repo on github)
+*/
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,127 @@
 // YOUR CODE HERE:
 
+$(document).ready(function () {
+  $('#send').on('submit', function (event) {
+    app.handleSubmit();
+    $('#message').val('');
+    event.preventDefault();
+  });
+  $('#addroom').on('click', function () {
+    var roomName = $('#roomname').val();
+    app.addRoom(roomName);
+  });
+  $('#roomSelect').on('change', function () {
+    app.appendMessages(app.messages);
+  });
+  app.init();
+});
+
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.messages;
+app.friends = {};
+
+app.init = function () {
+  app.fetch();
+};
+
+app.send = function (message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    success: function (data) {
+      var messages = data.results;
+      app.messages = messages;
+      app.appendMessages(messages);
+      setTimeout(app.fetch, 30000);
+    }
+  });
+};
+
+app.clearMessages = function () {
+  $('#chats').empty();
+};
+
+app.addMessage = function (message) {
+  var html = '<span data-username="';
+  html += message.username;
+  html += '">';
+  html += '<span class="username">';
+  html += message.username;
+  html += '</span>';
+  html += ': ';
+  html += app.sanitizeInput(message.text || '');
+  html += '</span>';
+  html += '<br />';
+  var $html = $(html);
+  $('#chats').append($html);
+  $html.on('click', function() {
+    app.addFriend(message.username);
+  });
+};
+
+app.addRoom = function (roomName) {
+  var html = '<option value="';
+  html += roomName;
+  html += '">';
+  html += roomName;
+  html += '</option>';
+  $('#roomSelect').append(html);
+  $('#roomSelect').val(roomName);
+  $('#roomSelect').trigger('change');
+};
+
+app.addFriend = function (friend) {
+  app.friends[friend] = true;
+  $('[data-username="'+friend+'"]').addClass('friend');
+};
+
+app.handleSubmit = function () {
+  var message = {};
+  message.username = $('#send #username').val();
+  message.text = $('#send #message').val();
+  message.roomname = $('#roomSelect').val();
+  app.send(message);
+};
+
+app.sanitizeInput = function (input) {
+  var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '')
+    .replace(/<[\/\!]*?[^<>]*?>/gi, '')
+    .replace(/<style[^>]*?>.*?<\/style>/gi, '')
+    .replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+  return output;
+};
+
+app.filterByRoom = function (messages) {
+  var roomName = $('#roomSelect').val();
+  if (!roomName) return messages;
+  return _.filter(messages, function(message) {
+    return message.roomname === roomName;
+  });
+};
+
+app.appendMessages = function (messages) {
+  app.clearMessages();
+  messages = app.filterByRoom(messages);
+  messages.forEach(function (message) {
+    app.addMessage(message);
+  });
+  $('[data-username="'+friend+'"]').addClass('friend');
+};
